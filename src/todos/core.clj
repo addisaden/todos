770a9cbcd1045e1dc3c13482e1bd06fdb76c6d6b
Adrian E <addis.aden@gmail.com>
2013-11-08 13:48:38 +0100

    created a lil demo of functionality in core

--- src/todos/core.clj
+++ src/todos/core.clj
@@ -1,8 +1,28 @@
 (ns todos.core
   (:gen-class)
+  (:require [todos.todo-item :as t-item])
   (:require [todos.storage :as storage]))
 
+(defn demo
+  "This is currently just a demo!"
+  []
+  (let
+    [todolist (t-item/todo-load-from-plain (storage/load-data
+                                             (((t-item/todo-create "main") :plain))
+                                             ))
+     sub-a    (t-item/todo-create "Privat")
+     sub-b    (t-item/todo-create "Arbeit")]
+    ((todolist :note-add) "Allgemeine Notizen")
+    ((todolist :todo-add) sub-a)
+    ((todolist :todo-add) sub-b)
+    ((sub-a :set-done) true)
+    ((sub-b :note-add) "arbeit arbeit")
+    ((todolist :print) "")
+    (println ((todolist :plain)))
+    ))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Start the demo ..."
   [& args]
-  (println (storage/load-data [])))
+  (demo))
+
