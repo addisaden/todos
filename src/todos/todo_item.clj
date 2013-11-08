c753ae40abed2f57f58c0fdb201b7516408e3bbe
Adrian E <addis.aden@gmail.com>
2013-11-08 11:51:13 +0100

    todo-item can have notes

--- src/todos/todo_item.clj
+++ src/todos/todo_item.clj
@@ -3,12 +3,15 @@
 (defn todo-create
   [n]
   (let [todo-name (atom n)
-        done (atom false)]
+        done      (atom false)
+        notes     (atom [])]
     {:name     (fn [] @todo-name)
      :done?    (fn [] @done)
+     :notes    (fn [] @notes)
      :rename   (partial reset! todo-name)
      :set-done (partial reset! done)
-     :plain    (fn [] {:name @todo-name :done? @done})
+     :add-note (partial swap! notes conj)
+     :plain    (fn [] {:name @todo-name :done? @done :notes @notes})
      }))
 
 ; (atom []) cant be saved! be aware to build a transformfunction
@@ -17,6 +20,8 @@
   [todo-plain]
   (let [item (todo-create (todo-plain :name))]
     ((item :set-done) (todo-plain :done?))
+    (doseq [note (todo-plain :notes)]
+      ((item :add-note) note))
     item
     ))
 
