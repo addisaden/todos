2b959e9fb69f49fb366c4df657b3609ff7fb1b9a
Adrian E <addis.aden@gmail.com>
2013-11-08 12:28:52 +0100

    todo-items can now have sub-todos

--- src/todos/todo_item.clj
+++ src/todos/todo_item.clj
@@ -4,17 +4,27 @@
   [n]
   (let [todo-name (atom n)
         done      (atom false)
-        notes     (atom [])]
-    {:name     (fn [] @todo-name)           ; fn Get name   []
-     :done?    (fn [] @done)                ; fn is done?   []
-     :notes    (fn [] @notes)               ; fn Get notes  []
-     :rename   (partial reset! todo-name)   ; fn Rename     [string]
-     :set-done (partial reset! done)        ; fn set done   [true/false]
-     :add-note (partial swap! notes conj)   ; fn add a note [string]
-     :plain    (fn [] {:name @todo-name     ; fn transform atoms in normal data
-                       :done? @done         ;    atoms cant be saved!
-                       :notes @notes
-                       })
+        notes     (atom [])
+        sub-todos (atom [])]
+    {:name        (fn [] @todo-name)           ; fn Get name
+     :rename      (partial reset! todo-name)   ; fn Rename     [string]
+
+     :done?       (fn [] @done)                ; fn is done?
+     :set-done    (partial reset! done)        ; fn set done   [true/false]
+
+     :notes       (fn [] @notes)               ; fn Get notes
+     :add-note    (partial swap! notes conj)   ; fn add a note [string]
+
+     :sub-todos   (fn [] @sub-todos)           ; fn Get Sub-Todolist
+     :sub-add     (fn [sub-todo-item]          ; fn Create a new Sub-Todo [todo-item]
+                    (swap! sub-todos conj sub-todo-item))
+
+     :plain       (fn [] {:name @todo-name     ; fn transform atoms in normal data
+                          :done? @done         ;    atoms cant be saved!
+                          :notes @notes
+                          :sub-todos (vec (for [i @sub-todos]
+                                       ((i :plain))))
+                          })
      }))
 
 ; (atom []) cant be saved! be aware to build a transformfunction
@@ -25,6 +35,8 @@
     ((item :set-done) (todo-plain :done?))
     (doseq [note (todo-plain :notes)]
       ((item :add-note) note))
+    (doseq [sub-item (todo-plain :sub-todos)]
+      ((item :sub-add) (todo-load-from-plain sub-item)))
     item
     ))
 
