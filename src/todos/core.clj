104ad61d669f36072edf73b8b5717fc5dd91bf8c
Adrian E <addis.aden@gmail.com>
2013-11-10 13:37:02 +0100

    basic command repl works

--- src/todos/core.clj
+++ src/todos/core.clj
@@ -23,8 +23,33 @@
     (storage/save-data ((todolist :plain)))
     ))
 
+(def todolist
+  (t-item/todo-load-from-plain
+    (storage/load-data
+      (((t-item/todo-create "< t o d o s >") :plain)) ; if file cant be read this is the default.
+      )))
+
+(def current-stack (atom '() )) ; this have the subpath of opened todos. (subpath in todolist)
+
+(defn command-repl
+  []
+  (do
+    (doseq [i @current-stack]
+      (println (format "> %s <" ((i :name)) )))
+    (let [[cmd & args] (inp/user-cmd-input ">> ")]
+      (cond
+        (= cmd "ls")
+        (( ;evaluate the print of current todolist
+          (or (first @current-stack) todolist)
+          :print) "")
+        )
+      ; exit clause
+      (if (not= cmd "exit")
+        (recur))
+      )))
+
 (defn -main
   "Start the demo ..."
   [& args]
-  (demo))
+  (command-repl))
 
