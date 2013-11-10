adf8247f3172b2bc2df15889782f6f4858f9b95e
Adrian E <addis.aden@gmail.com>
2013-11-10 15:41:43 +0100

    finished 0.1.0

--- src/todos/core.clj
+++ src/todos/core.clj
@@ -18,7 +18,7 @@
   (do
     (println)
     (doseq [i @current-stack]
-      (print (format "%s <- " ((i :name)) )))
+      (print (format "[%s] %s <- " (if ((i :done?)) "x" " ") ((i :name)) )))
     (println ((todolist :name)))
     (let [[cmd & args] (inp/user-cmd-input ">> ")
           current-todo (or (first @current-stack) todolist)
@@ -39,7 +39,10 @@
         ; show
         ;
         (= cmd "ls")
-        ((current-todo :print) "")
+        (do
+          (doseq [n ((current-todo :notes))] (println (format "- %s" n)))
+          (doseq [t ((current-todo :todos))] ((t :print) ""))
+          )
         ;
         (= cmd "plain")
         (println ((current-todo :plain)))
@@ -67,13 +70,16 @@
         ;
         ; manipulation
         ;
+        (= cmd "rename")
+        ((current-todo :rename) joined-args)
+        ;
         (= cmd "done")
         (if (empty? args)
           ((current-todo :set-done) true)
           (let [m (find-by-name ((current-todo :todos)) joined-args)]
             (if m
               ((m :set-done) true)
-              (println "Konnte Todo nicht finden")
+              (println "Cant find this todo.")
               )))
         ;
         (= cmd "undone")
@@ -82,7 +88,7 @@
           (let [m (find-by-name ((current-todo :todos)) joined-args)]
             (if m
               ((m :set-done) false)
-              (println "Konnte Todo nicht finden")
+              (println "Cant find this todo.")
               )))
         ;
         ; navigation
@@ -91,7 +97,7 @@
         (let [ft (first ((current-todo :todos)))]
           (if ft
             (swap! current-stack conj ft)
-            (println "Es existieren keine Subtodos.")
+            (println "Subtodos doesnt exists.")
             ))
         ;
         (and (= cmd "cd") (= joined-args ".."))
@@ -101,16 +107,51 @@
         (let [m (find-by-name ((current-todo :todos)) joined-args)]
           (if m
             (swap! current-stack conj m)
-            (println (format "Es existiert kein Subtodo namens \"%s\"." joined-args))
+            (println (format "There is no subtodo with the name \"%s\"." joined-args))
             ))
         ;
         (= cmd "nth")
         (let [n (- (read-string joined-args) 1)]
           (if (and (integer? n) (>= n 0) (< n (count ((current-todo :todos)))))
             (swap! current-stack conj (nth ((current-todo :todos)) n))
-            (println "Id ist ungültig. Es gibt nur" (count ((current-todo :todos))))
+            (println "Id doesnt exist. There are only" (count ((current-todo :todos))) "ids")
             ))
-        )
+        ;
+        ; help
+        ;
+        (not= cmd "exit")
+        (do
+          (println "commands:")
+          (println)
+          (println "exit           - quit the app")
+          (println)
+          (println "s h o w")
+          (println)
+          (println "ls             - list the todos (on current navigation)")
+          (println "plain          - shows the raw data")
+          (println)
+          (println "c r e a t e")
+          (println)
+          (println "create <str>   - create a new todo on the current todo")
+          (println "note <str>     - create a note on current todo")
+          (println)
+          (println "r e m o v e")
+          (println)
+          (println "remove <str>   - remove todolist with the name of str")
+          (println "rm-note <str>  - remove the note with the content of str")
+          (println)
+          (println "m a n i p u l a t i o n")
+          (println)
+          (println "rename <str>   - rename the current todo")
+          (println "done [<str>]   - set todo (str or current) to done")
+          (println "undone [<str>] - reverse of done")
+          (println)
+          (println "n a v i g a t i o n")
+          (println)
+          (println "cd ..          - navigate to the parent todo")
+          (println "cd <str>       - navigate to todo with given name")
+          (println "nth <n>        - navigate to todo with position of n (1..)")
+          ))
       ; exit clause
       (if (not= cmd "exit")
         (recur))
@@ -120,6 +161,12 @@
   "Start the demo ..."
   [& args]
   (do
+    (println)
+    (println "+++++  +++  ++++   +++   ++++")
+    (println "  +   +   +  +  + +   + +")
+    (println "  +   +   +  +  + +   +  +++")
+    (println "  +   +   +  +  + +   +     +")
+    (println "  +    +++  ++++   +++  ++++")
     (command-repl)
     (storage/save-data ((todolist :plain)))
     ))
